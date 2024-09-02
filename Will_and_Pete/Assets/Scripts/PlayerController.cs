9d7aa3c863ce68126d48a8409274d62248fa5b4b
Talatin <john.peter.hansen@live.de>
2024-09-02 18:50:04 +0200

    added inputs for abilities and backpack handling

--- Will_and_Pete/Assets/Scripts/PlayerController.cs
+++ Will_and_Pete/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,21 +9,58 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
-        playerState = GetComponent<PlayerState>();
-        playerInput = GetComponent<PlayerInput>();
-        playerMovement = GetComponent<IPlayerMovement>();
+        GetComponents();
     }
-  
+
     private void Update()
+    {
+        HandleUpdateInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleFixedUpdateInputs();
+    }
+
+    private void HandleUpdateInputs()
     {
         if (playerInput.JumpInput)
         {
             playerMovement.Jump(playerInput, playerState);
             playerInput.JumpInput = false;
         }
+
+        if (playerInput.BackPackInput)
+        {
+            Debug.Log($"{gameObject.name} used Backpack");
+            playerInput.BackPackInput = false;
+        }
+        if (playerInput.BackPackHeld)
+        {
+            Debug.Log($"{gameObject.name} held Backpack");
+            playerInput.BackPackHeld = false;
+        }
+        if (playerInput.AbilityOneInput)
+        {
+            Debug.Log($"{gameObject.name} used ability 1");
+            playerInput.AbilityOneInput = false;
+        }
+        if (playerInput.AbilityTwoInput)
+        {
+            Debug.Log($"{gameObject.name} used ability 2");
+            playerInput.AbilityTwoInput = false;
+        }
     }
-    private void FixedUpdate()
+
+    private void HandleFixedUpdateInputs()
     {
         playerMovement.UpdateMovement(playerInput, playerState);
+    }
+
+    private void GetComponents()
+    {
+        playerState = GetComponent<PlayerState>();
+        playerInput = GetComponent<PlayerInput>();
+        playerMovement = GetComponent<IPlayerMovement>();
     }
 }
--- Will_and_Pete/Assets/Scripts/PlayerInput.cs
+++ Will_and_Pete/Assets/Scripts/PlayerInput.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,6 +6,13 @@ public class PlayerInput : MonoBehaviour
     public Vector2 MovementInput;
     public bool JumpInput;
     public bool JumpHeld;
+
+    public bool AbilityOneInput;
+    public bool AbilityTwoInput;
+    
+    public bool BackPackInput;
+    public bool BackPackHeld;
+    
     //public bool AimingInput;
     //public bool FireInput;
 
@@ -15,10 +20,31 @@ public class PlayerInput : MonoBehaviour
     {
         MovementInput = context.ReadValue<Vector2>();
     }
-
+    
     public void OnJump(InputAction.CallbackContext context)
     {
         JumpInput = context.action.triggered;
         JumpHeld = context.action.triggered;
     }
+    
+    public void OnAbilityOne(InputAction.CallbackContext context)
+    {
+        AbilityOneInput = context.action.triggered;
+    }
+
+    public void OnAbilityTwo(InputAction.CallbackContext context)
+    {
+        AbilityTwoInput = context.action.triggered;
+    }
+
+    public void OnBackPack(InputAction.CallbackContext context)
+    {
+        BackPackInput = context.action.triggered;
+    }
+    
+    public void OnBackPackHold(InputAction.CallbackContext context)
+    {
+        BackPackHeld = context.action.triggered;
+    }
+
 }
