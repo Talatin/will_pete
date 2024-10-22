d942317b250a4e7504de4554cde2d43c5aecf4fa
John Hansen <john.peter.hansen@live.de>
2024-10-22 11:22:35 +0200

    cleaned up health and damage component
    isDowned as a bool for player checking

--- Will_and_Pete/Assets/Scripts/Player/PlayerHealth.cs
+++ Will_and_Pete/Assets/Scripts/Player/PlayerHealth.cs
@@ -6,65 +6,18 @@ namespace Assets.Scripts.Player
     public class PlayerHealth : MonoBehaviour
     {
         public enum DamageType { Health, Knockback, InstaKill, reset }
+        public bool isDowned {  get; private set; }
         private Vector3 lastStandingPosition;
         public Vector3 LastStandingPosition { set { lastStandingPosition = value; } }
-        private Rigidbody2D rb;
-        [SerializeField] private float knockbackPower;
-        [SerializeField] private Vector2 knockbackOffset;
-
-        private void Awake()
-        {
-            rb = GetComponent<Rigidbody2D>();
-        }
-
+       
         public void TakeDamage(DamageType type, Transform source)
         {
-            switch (type)
-            {
-                case DamageType.Health:
-                    break;
-                case DamageType.Knockback:
-                    KnockbackDamage(source);
-                    break;
-                case DamageType.InstaKill:
-                    InstaKill();
-                    break;
-                case DamageType.reset:
-                    ResetPlayer();
-                    break;
-                default:
-                    break;
-            }
+            isDowned = true;
         }
 
-        private void HealthDamage()
+        public void HelpBackUp()
         {
-
+            isDowned = false;
         }
-
-        private void KnockbackDamage(Transform source)
-        {
-            Debug.Log($"Knockbacked by {source.name}");
-            Vector3 dir = (source.transform.position - transform.position).normalized;
-            float xKnockback = Vector2.Dot(dir, transform.right) < 0 ? 1 : -1;
-            Vector2 knockDir = new Vector2(xKnockback, knockbackOffset.y).normalized;
-
-            rb.AddForce(knockDir * knockbackPower, ForceMode2D.Impulse);
-        }
-        private void InstaKill()
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        }
-        private void ResetPlayer()
-        {
-            rb.velocity = Vector2.zero;
-            transform.position = lastStandingPosition;
-        }
-
-        private void Update()
-        {
-
-        }
-
     }
 }
