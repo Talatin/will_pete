bf130870a1c4d0a9a661a0fe2da945ce5fd0f142
Talatin <john.peter.hansen@live.de>
2024-11-20 11:59:55 +0100

    loading scene behaviour

---
+++ Will_and_Pete/Assets/Scripts/Scene_Management/LoadingSceneBehaviour.cs
@@ -0,0 +1,55 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneBehaviour : MonoBehaviour
+{
+    LoadingAnimationBehaviour loadingAnimationBehaviour;
+    AsyncOperation operation;
+    private float currentLoadingTime;
+    private float loadingPerc;
+
+    public float AdditionalLoadingTime;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        loadingAnimationBehaviour = GetComponent<LoadingAnimationBehaviour>();
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentLoadingTime += Time.deltaTime;
+        loadingPerc = currentLoadingTime / AdditionalLoadingTime;
+    }
+
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(SceneLoader.NextLevelName);
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            loadingPerc *= asyncOperation.progress;
+            loadingAnimationBehaviour.UpdateLoadingVisuals(loadingPerc);
+
+            // Check if the load has finished
+            if (loadingPerc >= 0.9f)
+            {
+                asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+}
---
+++ Will_and_Pete/Assets/Scripts/Scene_Management/SceneLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public static class SceneLoader
+    {
+        private const string LOADING_SCENE_NAME = "LoadingScene";
+        private static string nextLevelName;
+        public static string NextLevelName { get { return nextLevelName; } }
+
+        public static void LoadTransitionScene()
+        {
+            if (SceneManager.GetActiveScene().name != LOADING_SCENE_NAME)
+            {
+                SceneManager.LoadScene(LOADING_SCENE_NAME);
+            }
+        }
+
+        public static void LoadScene(string sceneName)
+        {
+            nextLevelName = sceneName;
+            LoadTransitionScene();
+        }
+    }
+}
